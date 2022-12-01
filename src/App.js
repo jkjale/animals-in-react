b979d5d7292999b6669aca4945ec276465a22a24
Jake Lee <leejakejh@gmail.com>
2022-12-01 08:44:47 -0800

    animals showing up

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import AnimalShow from './AnimalShow';
+import {useState} from 'react';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index}/>
+  })
+
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal!!!</button>
+      <div id='animalsDiv'>{renderedAnimals}</div>
+    </div>
+  );
+}
+
+
+
+export default App;
